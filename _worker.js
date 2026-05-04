d2b862ea7ae700f4ba069a630b8b9b51fbdfa3f6
Trịnh Hg <onepiecechapter@gmail.com>
2026-05-04 23:09:46 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,24 +1,16 @@
-// v1.3 - Chỉ Proxy, không check KV ở đây
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    // Bẻ lái ngầm sang con Worker API
+    // Nếu là Link Sub hoặc API -> Bẻ lái ngầm sang con Worker Test (Nơi chứa Database)
     if (url.pathname.startsWith("/v1") || url.pathname.startsWith("/v2") || url.pathname.startsWith("/api")) {
       const workerUrl = new URL(request.url);
-      workerUrl.hostname = "vpn-api.doicucden.workers.dev";
-      
-      const newRequest = new Request(workerUrl.toString(), {
-        method: request.method,
-        headers: request.headers,
-        body: (request.method === 'GET' || request.method === 'HEAD') ? null : request.body,
-        redirect: "manual"
-      });
-      
+      workerUrl.hostname = "vpn-worker-test.doicucden.workers.dev";
+      const newRequest = new Request(workerUrl.toString(), request);
       return fetch(newRequest);
     }
 
-    // Phục vụ giao diện tĩnh (index.html, admin.html,...)
+    // Nếu khách truy cập bình thường -> Phục vụ file giao diện tĩnh
     return env.ASSETS.fetch(request);
   }
 };
